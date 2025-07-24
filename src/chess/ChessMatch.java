ab425167d7ab9f9adfc950f59e45f69fcec31aec
unknown <yagodev93@gmail.com>
2025-07-24 05:24:47 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -111,7 +111,8 @@ public class ChessMatch {
 		}
 	
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece)board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		
